18c79af7b7e565bcbda16c4229f0ca144baee51a
Valentina Ponamareva <valiazin@gmail.com>
2021-02-09 18:10:00 +0300

    feat: add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import dateFilter from '@/filters/date.filter';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuelidate);
 Vue.filter('dateFilter', dateFilter);
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
@@ -34,8 +34,29 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'login',
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+  },
+  methods: {
+    submitHandler() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
